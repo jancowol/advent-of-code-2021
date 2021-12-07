bda35f3c887c9047e12feb0e0e35c224097e026c
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-07 06:59:38 +0200

    solve day 6.2

--- tests/test_advent.py
+++ tests/test_advent.py
@@ -1,3 +1,4 @@
+import time
 from advent import advent
 from advent import bingoinput
 
@@ -527,21 +528,73 @@ def test_day_6_1_model_fish_school():
     end_fish_clocks = [f.clock for f in result_school]
     assert end_fish_clocks == expected_after_18_days
 
+
 def test_against_advent_example():
     fish_clocks = [3, 4, 3, 1, 2]
 
-    result_school = model_school_from_clocks(fish_clocks)
+    result_school = model_school_from_clocks(fish_clocks, 80)
 
     assert len(result_school) == 5934
 
-def model_school_from_clocks(fish_clocks):
-    school = [Fish(clock) for clock in fish_clocks]
-    return model_school_spawn(school, 80)
 
-def test_solve_day_6_1():
-    puzzle_input = [1,1,3,5,1,3,2,1,5,3,1,4,4,4,1,1,1,3,1,4,3,1,2,2,2,4,1,1,5,5,4,3,1,1,1,1,1,1,3,4,1,2,2,5,1,3,5,1,3,2,5,2,2,4,1,1,1,4,3,3,3,1,1,1,1,3,1,3,3,4,4,1,1,5,4,2,2,5,4,5,2,5,1,4,2,1,5,5,5,4,3,1,1,4,1,1,3,1,3,4,1,1,2,4,2,1,1,2,3,1,1,1,4,1,3,5,5,5,5,1,2,2,1,3,1,2,5,1,4,4,5,5,4,1,1,3,3,1,5,1,1,4,1,3,3,2,4,2,4,1,5,5,1,2,5,1,5,4,3,1,1,1,5,4,1,1,4,1,2,3,1,3,5,1,1,1,2,4,5,5,5,4,1,4,1,4,1,1,1,1,1,5,2,1,1,1,1,2,3,1,4,5,5,2,4,1,5,1,3,1,4,1,1,1,4,2,3,2,3,1,5,2,1,1,4,2,1,1,5,1,4,1,1,5,5,4,3,5,1,4,3,4,4,5,1,1,1,2,1,1,2,1,1,3,2,4,5,3,5,1,2,2,2,5,1,2,5,3,5,1,1,4,5,2,1,4,1,5,2,1,1,2,5,4,1,3,5,3,1,1,3,1,4,4,2,2,4,3,1,1]
-    result = model_school_from_clocks(puzzle_input)
-    print(f'***** day 6.1 {len(result)}')
+def model_school_from_clocks(fish_clocks, days):
+    school = [Fish(clock) for clock in fish_clocks]
+    return model_school_spawn(school, days)
+
+# def test_solve_day_6_1():
+#     puzzle_input = [1,1,3,5,1,3,2,1,5,3,1,4,4,4,1,1,1,3,1,4,3,1,2,2,2,4,1,1,5,5,4,3,1,1,1,1,1,1,3,4,1,2,2,5,1,3,5,1,3,2,5,2,2,4,1,1,1,4,3,3,3,1,1,1,1,3,1,3,3,4,4,1,1,5,4,2,2,5,4,5,2,5,1,4,2,1,5,5,5,4,3,1,1,4,1,1,3,1,3,4,1,1,2,4,2,1,1,2,3,1,1,1,4,1,3,5,5,5,5,1,2,2,1,3,1,2,5,1,4,4,5,5,4,1,1,3,3,1,5,1,1,4,1,3,3,2,4,2,4,1,5,5,1,2,5,1,5,4,3,1,1,1,5,4,1,1,4,1,2,3,1,3,5,1,1,1,2,4,5,5,5,4,1,4,1,4,1,1,1,1,1,5,2,1,1,1,1,2,3,1,4,5,5,2,4,1,5,1,3,1,4,1,1,1,4,2,3,2,3,1,5,2,1,1,4,2,1,1,5,1,4,1,1,5,5,4,3,5,1,4,3,4,4,5,1,1,1,2,1,1,2,1,1,3,2,4,5,3,5,1,2,2,2,5,1,2,5,3,5,1,1,4,5,2,1,4,1,5,2,1,1,2,5,4,1,3,5,3,1,1,3,1,4,4,2,2,4,3,1,1]
+#     result = model_school_from_clocks(puzzle_input, 80)
+#     print(f'***** day 6.1 {len(result)}')
+
+# def test_solve_day_6_2():
+#     puzzle_input = [1,1,3,5,1,3,2,1,5,3,1,4,4,4,1,1,1,3,1,4,3,1,2,2,2,4,1,1,5,5,4,3,1,1,1,1,1,1,3,4,1,2,2,5,1,3,5,1,3,2,5,2,2,4,1,1,1,4,3,3,3,1,1,1,1,3,1,3,3,4,4,1,1,5,4,2,2,5,4,5,2,5,1,4,2,1,5,5,5,4,3,1,1,4,1,1,3,1,3,4,1,1,2,4,2,1,1,2,3,1,1,1,4,1,3,5,5,5,5,1,2,2,1,3,1,2,5,1,4,4,5,5,4,1,1,3,3,1,5,1,1,4,1,3,3,2,4,2,4,1,5,5,1,2,5,1,5,4,3,1,1,1,5,4,1,1,4,1,2,3,1,3,5,1,1,1,2,4,5,5,5,4,1,4,1,4,1,1,1,1,1,5,2,1,1,1,1,2,3,1,4,5,5,2,4,1,5,1,3,1,4,1,1,1,4,2,3,2,3,1,5,2,1,1,4,2,1,1,5,1,4,1,1,5,5,4,3,5,1,4,3,4,4,5,1,1,1,2,1,1,2,1,1,3,2,4,5,3,5,1,2,2,2,5,1,2,5,3,5,1,1,4,5,2,1,4,1,5,2,1,1,2,5,4,1,3,5,3,1,1,3,1,4,4,2,2,4,3,1,1]
+#     result = model_school_from_clocks(puzzle_input, 256)
+#     print(f'***** day 6.2 {len(result)}')
+
+
+def test_solve_day_6_2_approach2():
+    input = [1, 1, 3, 5, 1, 3, 2, 1, 5, 3, 1, 4, 4, 4, 1, 1, 1, 3, 1, 4, 3, 1, 2, 2, 2, 4, 1, 1, 5, 5, 4, 3, 1, 1, 1, 1, 1, 1, 3, 4, 1, 2, 2, 5, 1, 3, 5, 1, 3, 2, 5, 2, 2, 4, 1, 1, 1, 4, 3, 3, 3, 1, 1, 1, 1, 3, 1, 3, 3, 4, 4, 1, 1, 5, 4, 2, 2, 5, 4, 5, 2, 5, 1, 4, 2, 1, 5, 5, 5, 4, 3, 1, 1, 4, 1, 1, 3, 1, 3, 4, 1, 1, 2, 4, 2, 1, 1, 2, 3, 1, 1, 1, 4, 1, 3, 5, 5, 5, 5, 1, 2, 2, 1, 3, 1, 2, 5, 1, 4, 4, 5, 5, 4, 1, 1, 3, 3, 1, 5, 1, 1, 4, 1, 3, 3, 2, 4, 2, 4,
+             1, 5, 5, 1, 2, 5, 1, 5, 4, 3, 1, 1, 1, 5, 4, 1, 1, 4, 1, 2, 3, 1, 3, 5, 1, 1, 1, 2, 4, 5, 5, 5, 4, 1, 4, 1, 4, 1, 1, 1, 1, 1, 5, 2, 1, 1, 1, 1, 2, 3, 1, 4, 5, 5, 2, 4, 1, 5, 1, 3, 1, 4, 1, 1, 1, 4, 2, 3, 2, 3, 1, 5, 2, 1, 1, 4, 2, 1, 1, 5, 1, 4, 1, 1, 5, 5, 4, 3, 5, 1, 4, 3, 4, 4, 5, 1, 1, 1, 2, 1, 1, 2, 1, 1, 3, 2, 4, 5, 3, 5, 1, 2, 2, 2, 5, 1, 2, 5, 3, 5, 1, 1, 4, 5, 2, 1, 4, 1, 5, 2, 1, 1, 2, 5, 4, 1, 3, 5, 3, 1, 1, 3, 1, 4, 4, 2, 2, 4, 3, 1, 1]
+
+    def timed_test():
+        result = count_many_generations(input)
+        print(result)
+        assert result == 1631629590423
+
+    timed(timed_test)
+
+
+def count_many_generations(input):
+    dict = {}
+    for clock in input:
+        fish_count_for_clock = dict.get(clock, 0)
+        dict.update({clock: fish_count_for_clock + 1})
+
+    print('********************')
+    dict2 = dict
+    print(dict2)
+    for i in range(256):
+        dict2[-1] = dict2.get(0, 0)
+        dict2[0] = dict2.get(1, 0)
+        dict2[1] = dict2.get(2, 0)
+        dict2[2] = dict2.get(3, 0)
+        dict2[3] = dict2.get(4, 0)
+        dict2[4] = dict2.get(5, 0)
+        dict2[5] = dict2.get(6, 0)
+        dict2[6] = dict2.get(7, 0)
+        dict2[7] = dict2.get(8, 0)
+        dict2[8] = dict2[-1]
+        dict2[6] = dict2[6] + dict2[-1]
+        print(dict2)
+    result = sum(dict2.values()) - dict2[-1]
+    return result
+
+
+def timed(func):
+    start = time.time()
+    func()
+    end = time.time()
+    print((end - start) * 1000)
 
 
 def model_school_spawn(initial_school, simulation_days):
@@ -551,3 +604,5 @@ def model_school_spawn(initial_school, simulation_days):
             foo.day_tick() for foo in school) if new_fish_age != None]
         school += new_fish
     return school
+
+# 1,631,629,590,423
