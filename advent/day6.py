df5e66aaf167fab5a845e9b48e509d25ad24047a
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-07 20:52:15 +0200

    clean up old irrelevant day 6 code

--- advent/day6.py
+++ advent/day6.py
@@ -1,23 +1,4 @@
-
-class Fish():
-    def __init__(self, clock):
-        self.clock = clock
-
-    def day_tick(self):
-        self.clock -= 1
-        if(self.clock == -1):
-            self.clock = 6
-            return 8
-        # if(self.clock )
-        # return 8
-
-
-def model_school_from_clocks(fish_clocks, days):
-    school = [Fish(clock) for clock in fish_clocks]
-    return model_school_spawn(school, days)
-
-
-def count_many_generations(input):
+def count_many_generations(input, days):
     dict = {}
     for clock in input:
         fish_count_for_clock = dict.get(clock, 0)
@@ -26,7 +7,7 @@ def count_many_generations(input):
     print('********************')
     dict2 = dict
     print(dict2)
-    for i in range(256):
+    for i in range(days):
         dict2[-1] = dict2.get(0, 0)
         dict2[0] = dict2.get(1, 0)
         dict2[1] = dict2.get(2, 0)
@@ -41,12 +22,3 @@ def count_many_generations(input):
         print(dict2)
     result = sum(dict2.values()) - dict2[-1]
     return result
-
-
-def model_school_spawn(initial_school, simulation_days):
-    school = initial_school
-    for i in range(simulation_days):
-        new_fish = [Fish(new_fish_age) for new_fish_age in (
-            foo.day_tick() for foo in school) if new_fish_age != None]
-        school += new_fish
-    return school
