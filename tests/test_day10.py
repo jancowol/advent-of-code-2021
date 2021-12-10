d9373c6b94d9c8a897fae4c1dc3143a764e0d68a
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-10 08:19:56 +0200

    move code into separate file

--- tests/test_day10.py
+++ tests/test_day10.py
@@ -1,3 +1,4 @@
+import advent.day10 as d10
 from advent.file_utils import read_file
 from functools import reduce
 
@@ -13,17 +14,12 @@ test_input = [
     '<{([([[(<>()){}]>(<<{{',
     '<{([{{}}[<[[[<>{}]]]>[]]']
 
-chunk_open = ['[', '(', '{', '<']
-chunk_close = [']', ')', '}', '>']
-error_score = {')': 3, ']': 57, '}': 1197, '>': 25137}
-points_lookup = {')': 1, ']': 2, '}': 3, '>': 4}
-
 
 def test_one():
     input = read_file('day10-input')
     corrupted_lines = find_corrupted_lines(input)
 
-    scores = [error_score[x[1]] for x in corrupted_lines]
+    scores = [d10.error_score[x[1]] for x in corrupted_lines]
     total_score = sum(scores)
     print(scores)
     print(total_score)
@@ -34,9 +30,9 @@ def find_corrupted_lines(input):
     corrupted_lines = []
     for line in input:
         for char in line:
-            if char in chunk_open:
+            if char in d10.chunk_open:
                 stack.append(char)
-            if char in chunk_close:
+            if char in d10.chunk_close:
                 last_open_char = stack.pop()
                 if(last_open_char != inverse_of_close(char)):
                     corrupted_lines.append((line, char))
@@ -67,24 +63,15 @@ def test_calc_completion_score():
     mid_index = round(len(sorted_totals) / 2)
     print(sorted_totals[mid_index])
 
-    # for line in incomplete_lines:
-    #     line_completion, completion_score = complete_and_score_line(line)
-        # print(f'{"".join(line_completion)}: {completion_score}')
 
 def complete_and_score_line(line):
     line_completion = find_line_completion_chars(line)
     completion_score = calc_completion_score(line_completion)
-    return line_completion,completion_score
-        # print(f'line completion score: {completion_score}')
-
-    # input = ti
-    # points, total = calc_completion_score(input)
-    # print(total)
-    # print(points)
+    return line_completion, completion_score
 
 
 def calc_completion_score(test_input):
-    points = [points_lookup[x] for x in test_input]
+    points = [d10.points_lookup[x] for x in test_input]
     total = reduce(lambda x, y: (x*5) + y, points, 0)
     return total
 
@@ -92,19 +79,19 @@ def calc_completion_score(test_input):
 def find_line_completion_chars(line):
     stack = []
     for char in line:
-        if char in chunk_open:
+        if char in d10.chunk_open:
             stack.append(char)
-        if char in chunk_close:
+        if char in d10.chunk_close:
             stack.pop()
     line_completion = [inverse_of_open(x) for x in reversed(stack)]
     return line_completion
 
 
 def inverse_of_close(char):
-    index = chunk_close.index(char)
-    return chunk_open[index]
+    index = d10.chunk_close.index(char)
+    return d10.chunk_open[index]
 
 
 def inverse_of_open(char):
-    index = chunk_open.index(char)
-    return chunk_close[index]
+    index = d10.chunk_open.index(char)
+    return d10.chunk_close[index]
