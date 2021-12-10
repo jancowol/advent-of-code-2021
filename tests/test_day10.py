aef6aedc9a7e4b5b2f3bfa7e17020385985bfee6
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-10 08:17:03 +0200

    solve day 10

--- tests/test_day10.py
+++ tests/test_day10.py
@@ -1,4 +1,5 @@
 from advent.file_utils import read_file
+from functools import reduce
 
 test_input = [
     '[({(<(())[]>[[{[]{<()<>>',
@@ -15,6 +16,7 @@ test_input = [
 chunk_open = ['[', '(', '{', '<']
 chunk_close = [']', ')', '}', '>']
 error_score = {')': 3, ']': 57, '}': 1197, '>': 25137}
+points_lookup = {')': 1, ']': 2, '}': 3, '>': 4}
 
 
 def test_one():
@@ -52,8 +54,39 @@ def test_two():
         print(line_completion)
 
 
-def def_calc_completion_score():
-    pass
+def test_calc_completion_score():
+    print('---------------')
+    # ti = ['}', '}', ']', ']', ')', '}', ')', ']']
+
+    input = read_file('day10-input')
+    corrupted_lines = [line[0] for line in find_corrupted_lines(input)]
+    incomplete_lines = [x for x in input if x not in corrupted_lines]
+
+    totals = [complete_and_score_line(x)[1] for x in incomplete_lines]
+    sorted_totals = sorted(totals)
+    mid_index = round(len(sorted_totals) / 2)
+    print(sorted_totals[mid_index])
+
+    # for line in incomplete_lines:
+    #     line_completion, completion_score = complete_and_score_line(line)
+        # print(f'{"".join(line_completion)}: {completion_score}')
+
+def complete_and_score_line(line):
+    line_completion = find_line_completion_chars(line)
+    completion_score = calc_completion_score(line_completion)
+    return line_completion,completion_score
+        # print(f'line completion score: {completion_score}')
+
+    # input = ti
+    # points, total = calc_completion_score(input)
+    # print(total)
+    # print(points)
+
+
+def calc_completion_score(test_input):
+    points = [points_lookup[x] for x in test_input]
+    total = reduce(lambda x, y: (x*5) + y, points, 0)
+    return total
 
 
 def find_line_completion_chars(line):
@@ -63,7 +96,7 @@ def find_line_completion_chars(line):
             stack.append(char)
         if char in chunk_close:
             stack.pop()
-    line_completion = [inverse_of_open(x) for x in stack]
+    line_completion = [inverse_of_open(x) for x in reversed(stack)]
     return line_completion
 
 
