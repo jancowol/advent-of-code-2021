483660893165706fa0febac25a648d8d177e58da
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-10 06:38:29 +0200

    mess around with alternative approaches. It's a bloodbath really

--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from advent.file_utils import read_file
 from functools import reduce
+from collections import namedtuple
 
 test_input = [
     '2199943210',
@@ -56,8 +57,6 @@ def test_can_filter_adjacents_to_map_limits():
 
 
 def test_map_build():
-    x_limit = len(test_input[0]) - 1
-    y_limit = len(test_input) - 1
     m4 = build_map_from(test_input)
     print('******************* map building')
     print(m4)
@@ -76,19 +75,24 @@ def test_actual_data():
     print(result)
 
 
-def test_basins():
-    input = read_file('day9-input')
-    x_limit = len(input[0]) - 1
-    y_limit = len(input) - 1
-    map = build_map_from(input)
-    low_points = find_low_points(map)
+# def test_basins():
+#     input = read_file('day9-input')
+#     x_limit = len(input[0]) - 1
+#     y_limit = len(input) - 1
+#     map = build_map_from(input)
+#     low_points = find_low_points(map)
 
-    print('****************')
-    basins = [compute_basin(x, set([x]), x_limit, y_limit, map)
-              for x in low_points]
-    basin_sizes = sorted([len(basin) for basin in basins], reverse=True)[:3]
-    print(basin_sizes)
-    foo = reduce(lambda x, y: x * y, basin_sizes)
+#     basins = [compute_basin(x, set([x]), x_limit, y_limit, map)
+#               for x in low_points]
+#     basin_sizes = sorted([len(basin) for basin in basins], reverse=True)[:3]
+#     print(basin_sizes)
+#     foo = reduce(lambda x, y: x * y, basin_sizes)
+#     print('**************** part 2 final answer')
+#     print(foo)
+
+
+def test_named_tuple():
+    foo = IndexedItem(0, 0)
     print(foo)
 
 
@@ -125,14 +129,54 @@ def find_low_points(map):
     return low_points
 
 
+IndexedItem = namedtuple('IndexedItem', ['index', 'item'])
+
+
 def build_map_from(input):
     map_width = len(input[0])
     map_height = len(input)
 
-    map = list(zip(range(map_width), [reading for reading in input]))
+    lines_with_y = zip(range(map_height), input)
+
     m2 = [([(Point(x[0], row[0], map_width - 1, map_height - 1), int(x[1]))
-           for x in list(zip(range(len(row[1])), row[1]))]) for row in map]
+           for x in zip(range(len(row[1])), row[1])]) for row in lines_with_y]
 
     m3 = [item for sub in m2 for item in sub]
     m4 = {kv[0]: kv[1] for kv in m3}
     return m4
+
+
+def test_map_build3():
+    map_width = len(test_input[0])
+    map_height = len(test_input)
+
+# def test_map_build2():
+#     m4 = build_map_from2(test_input)
+#     foo = list(m4)
+#     print('~~~~~~~~~~~~~~~~~~~')
+#     for f in foo:
+#         # print(f)
+#         print(f[0])
+#         for b in f[1]:
+#             print('...')
+#             print(b)
+#     print(range(300))
+#     # print(foo)
+
+
+# def build_map_from2(input):
+#     map_width = len(input[0])
+#     map_height = len(input)
+
+#     lines_with_y = zip(range(map_height), input)
+#     # xxx = zip(range(map_width), x[1])
+#     lwy = ((x[0], map(lambda item: (item[0], int(item[1])), zip(range(map_width), x[1])))
+#            for x in lines_with_y)
+
+#     boo = list(lwy)
+#     # print('222222222222222222222 map building')
+#     # for item in boo:
+#     #     print(f'{item[0]}: {list(item[1])}')
+#     # lwy = (IndexedItem(index=x[0], item=[IndexedItem(0, reading)
+#     #        for reading in x[1]]) for x in zip(range(map_height), input))
+#     return boo
