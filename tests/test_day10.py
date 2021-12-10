66f076b82bcd4994483b4582c9edfcd109cf10c8
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-10 07:46:01 +0200

    find line completion chars

--- tests/test_day10.py
+++ tests/test_day10.py
@@ -43,9 +43,35 @@ def find_corrupted_lines(input):
 
 
 def test_two():
+    print('---------------test 2')
+    input = test_input
+    corrupted_lines = [line[0] for line in find_corrupted_lines(input)]
+    incomplete_lines = [x for x in input if x not in corrupted_lines]
+    for line in incomplete_lines:
+        line_completion = find_line_completion_chars(line)
+        print(line_completion)
+
+
+def def_calc_completion_score():
     pass
+
+
+def find_line_completion_chars(line):
+    stack = []
+    for char in line:
+        if char in chunk_open:
+            stack.append(char)
+        if char in chunk_close:
+            stack.pop()
+    line_completion = [inverse_of_open(x) for x in stack]
+    return line_completion
 
 
 def inverse_of_close(char):
     index = chunk_close.index(char)
     return chunk_open[index]
+
+
+def inverse_of_open(char):
+    index = chunk_open.index(char)
+    return chunk_close[index]
