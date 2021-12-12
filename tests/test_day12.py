13587029cc5d8e1866864c02f9c39e1bf1411e40
Janco Wolmarans <jancowol@users.noreply.github.com>
2021-12-12 20:54:51 +0200

    more refactoring

--- tests/test_day12.py
+++ tests/test_day12.py
@@ -47,7 +47,7 @@ def test_start():
         'end': ['A', 'b']
     }
 
-    result = explore_path(cave_map, 'start', 0, [])
+    result = explore_path(cave_map, 'start', [])
     for r in result:
         print(r)
 
@@ -55,7 +55,7 @@ def test_start():
 def test_real_input():
     map = input_to_map(input)
     print('------------------------------------------ start')
-    result = explore_path(map, 'start', 0, [])
+    result = explore_path(map, 'start', [])
     for r in result:
         print(r)
     print(len(result))
@@ -79,27 +79,6 @@ def test_can_identify_small_cave():
     assert is_small_cave(large_cave) == False
 
 
-def explore_path(cave_map, node, fork_count, path):
-    new_path = list(path)
-    new_path.append(node)
-
-    if(node == 'end'):
-        return [new_path]
-
-    exclude_nodes = [x for x in new_path if is_small_cave(x)]
-    connected_nodes = cave_map[node]
-    connected_nodes_to_visit = [
-        x for x in connected_nodes if x not in exclude_nodes]
-
-    subpaths = []
-    for connected_node in connected_nodes_to_visit:
-        subpath = explore_path(cave_map, connected_node,
-                               fork_count + 1, new_path)
-        subpaths.extend(subpath)
-
-    return subpaths
-
-
 def test_part2():
     test_input = [
         'fs-end',
@@ -121,41 +100,70 @@ def test_part2():
         'pj-fs',
         'start-RW']
     map = input_to_map(test_input)
-    result = explore_path2(map, 'start', [])
+    result = exp_path2(map, 'start', [])
 
     assert len(result) == 3509
 
 
-def explore_path2(cave_map, node, path):
+def explore_path(cave_map, node, path):
+    return walk_node_path(cave_map, node, path, non_visited_small_caves)
+
+
+def exp_path2(cave_map, node, path):
+    return walk_node_path(cave_map, node, path, visitable_nodes2)
+
+
+def walk_node_path(cave_map, node, path, node_filter):
     new_path = list(path)
     new_path.append(node)
 
     if(node == 'end'):
         return [new_path]
 
-    connected_nodes = filter(lambda x: x != 'start', cave_map[node])
-    visitable_nodes = filter(lambda x: can_visit_node(x, new_path), connected_nodes)
+    visitable_nodes = node_filter(cave_map, node, new_path)
 
     subpaths = []
     for connected_node in visitable_nodes:
-        subpath = explore_path2(cave_map, connected_node, new_path)
+        subpath = walk_node_path(
+            cave_map, connected_node, new_path, node_filter)
         subpaths.extend(subpath)
 
     return subpaths
 
 
-def can_visit_node(node, current_path):
+def non_visited_small_caves(cave_map, node, new_path):
+    visited_small_caves = list(filter(is_small_cave, new_path))
+    connected_nodes = cave_map[node]
+    return [node for node in connected_nodes if node not in visited_small_caves]
+
+
+def visitable_nodes2(cave_map, node, new_path):
+    connected_nodes = filter(lambda x: x != 'start', cave_map[node])
 
     rules = [
-        (not is_small_cave(node)),
-        (node == 'end'),
-        (current_path.count(node) == 0),
-        (not any_cave_visited_more_than_once(current_path))]
+        is_large_cave,
+        is_end_node,
+        node_not_yet_visited,
+        small_cave_visited_more_than_once
+    ]
 
-    return any(rules)
+    return filter(lambda node: can_visit_node(node, new_path, rules), connected_nodes)
 
 
-def any_cave_visited_more_than_once(current_path):
+def is_large_cave(node, path): return not is_small_cave(node)
+def is_end_node(node, path): return node == 'end'
+def node_not_yet_visited(node, path): return path.count(node) == 0
+
+
+def small_cave_visited_more_than_once(node, path):
+    return not any_small_cave_visited_more_than_once(path)
+
+
+def can_visit_node(node, current_path, rules):
+    return any(rule(node, current_path) for rule in rules)
+
+
+def any_small_cave_visited_more_than_once(current_path):
     small_caves_in_path = find_small_caves_in(current_path)
     cave_visit_counts = count_by_item(small_caves_in_path)
 
@@ -171,10 +179,6 @@ def count_by_item(nodes):
     for item in nodes:
         counts[item] = counts.get(item, 0) + 1
     return counts
-
-
-def groupBy(key, seq):
-    return reduce(lambda grp, val: grp[key(val)].append(val) or grp, seq, defaultdict(list))
 
 
 def is_small_cave(cave_name):
